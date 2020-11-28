dad3e20548b0d6ea6be77a6f8eb4ba8f478c87bf
“ <1506888205@qq.com>
2020-11-28 21:38:06 +0800

    冉菁 11.28

---
+++ program/xiangmu/app/public/personjsrj/personrj.js
@@ -0,0 +1,5 @@
+window.onload = function (){
+    let username = document.querySelector(".username");
+    let headpic = document.querySelector(".headpic");
+    let usertxt = document.querySelector(".usertxt");
+}
